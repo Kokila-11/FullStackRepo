a1cf20edde8c13afee4364ed7eb464e44b28a79e
kokila <bommidikoteswari@gmail.com>
2025-01-26 17:11:27 +0530

    Dev1--adding new things Branch1

--- GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
+++ GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -1,5 +1,18 @@
 package com.nt.upi;
 
+import java.util.Random;
+
 public class UPIPaymentOperations {
 
+	
+	public String doPayment(String upiId, double amount)
+	{
+		return amount+"is transferred to"+upiId;
+	}
+	
+	public String addrewars(String txId)
+	{
+		return txId+"has got"+new Random().nextInt(4000)+"reward points have come";
+	}
 }
+
